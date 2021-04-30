08a83e2e6aaf0dd1f49eaf22c642c3fb05711e09
Franky-Pham <frankpham1@gmail.com>
2021-04-30 01:41:40 -0400

    Version 1

--- oddInt/oddInt/Program.cs
+++ oddInt/oddInt/Program.cs
@@ -6,7 +6,32 @@ namespace oddInt
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 1, 5, 6, 6, 1, 6, 5};
+            int ans  = findOdd(array);
+            Console.WriteLine(ans);
+        }
+
+        static int findOdd(int[] array)
+        {
+            for(int i = 0; i<array.Length; i++)
+            {
+                int counter = 0;
+                for (int n = i; n<array.Length; n++)
+                {
+                    if (array[n] == array[i])
+                    {
+                        counter += 1;
+                    }
+                }
+                if (counter % 2 != 0)
+                {
+                return array[i];
+                }
+            }
+            return -1;
         }
     }
 }
+//Write a function that:
+//Given an array of integers, find the one that appears an odd number of times.
+//There will always be only one integer that appears an odd number of times.
