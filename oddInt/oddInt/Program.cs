bff44c838e7d559a78fb143197adf9ae6ce64f03
Franky-Pham <frankpham1@gmail.com>
2021-04-30 00:40:31 -0400

    Initial Commit

---
+++ oddInt/oddInt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oddInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
